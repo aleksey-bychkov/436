1a67c223ba28fb89459d6732d62f04796575851d
Brendan Collins <bcollin4@umd.edu>
2022-11-22 12:06:52 -0500

    Added side switching for incoming and outgoing messages

--- FireBaseChat/app/src/main/java/com/example/myapplication/RecyclerViewAdapter.kt
+++ FireBaseChat/app/src/main/java/com/example/myapplication/RecyclerViewAdapter.kt
@@ -6,19 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.annotation.NonNull
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
+
 
 class RecyclerViewAdapter
     (private var context: Context, private var list: ArrayList<Message>) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         //var username: TextView = itemView.findViewById(R.id.username)
-        var message: TextView = itemView.findViewById(R.id.user_message)
-        var dateTime: TextView = itemView.findViewById(R.id.user_message_date_time)
-        var messageID:TextView = itemView.findViewById(R.id.messageID)
-        var reportButton: FloatingActionButton = itemView.findViewById(R.id.reportButton)
+        var left: ConstraintLayout = itemView.findViewById(R.id.left)
+        var cardL: CardView = itemView.findViewById(R.id.cardLeft)
+        var messageL: TextView = itemView.findViewById(R.id.user_message_left)
+        var dateTimeL: TextView = itemView.findViewById(R.id.user_message_date_time_left)
+        var messageIDL:TextView = itemView.findViewById(R.id.messageID_left)
+        var reportButtonL: FloatingActionButton = itemView.findViewById(R.id.reportButton_left)
+
+        var right: ConstraintLayout = itemView.findViewById(R.id.right)
+        var cardR: CardView = itemView.findViewById(R.id.cardRight)
+        var messageR: TextView = itemView.findViewById(R.id.user_message_right)
+        var dateTimeR: TextView = itemView.findViewById(R.id.user_message_date_time_right)
+        var messageIDR:TextView = itemView.findViewById(R.id.messageID_right)
+        var reportButtonR: FloatingActionButton = itemView.findViewById(R.id.reportButton_right)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -28,16 +40,25 @@ class RecyclerViewAdapter
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         //holder.username.text = list.get(position).getUserEmail()
-        holder.message.text = list.get(position).getMsg()
-        holder.dateTime.text = list.get(position).getDateTime()
-        holder.messageID.text = list.get(position).getMessageID()
+
         if(FirebaseAuth.getInstance().uid == list.get(position).getUserID()){
-            holder.itemView.setBackgroundColor(parseColor("#CE93D8"))
-            holder.reportButton.visibility = View.INVISIBLE
+            holder.right.visibility = View.VISIBLE
+            holder.messageR.text = list.get(position).getMsg()
+            holder.dateTimeR.text = list.get(position).getDateTime()
+            holder.messageIDR.text = list.get(position).getMessageID()
+            holder.cardR.setBackgroundColor(parseColor("#CE93D8"))
+            holder.reportButtonR.visibility = View.GONE
+            holder.left.visibility = View.GONE
 
         } else {
-            holder.itemView.setBackgroundColor(parseColor("#D3D3D3"))
-            holder.reportButton.visibility = View.VISIBLE
+            holder.left.visibility = View.VISIBLE
+            holder.messageL.text = list.get(position).getMsg()
+            holder.dateTimeL.text = list.get(position).getDateTime()
+            holder.messageIDL.text = list.get(position).getMessageID()
+            holder.cardL.setBackgroundColor(parseColor("#D3D3D3"))
+            holder.reportButtonL.visibility = View.VISIBLE
+            holder.right.visibility = View.GONE
+
         }
 
 

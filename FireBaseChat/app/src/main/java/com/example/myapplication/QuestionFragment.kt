7a5504361e2cc8c596a9d4895a1b13db093c14cb
Brendan Collins <bcollin4@umd.edu>
2022-12-05 11:38:47 -0500

    Add view response to db

--- FireBaseChat/app/src/main/java/com/example/myapplication/QuestionFragment.kt
+++ FireBaseChat/app/src/main/java/com/example/myapplication/QuestionFragment.kt
@@ -4,18 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.myapplication.databinding.FragmentQuestionBinding
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class QuestionFragment(topicID: String) : Fragment() {
 
     private var topicID = topicID
-    private var responselist = ArrayList<String>()
+    private var responselist = ArrayList<Int>()
     private var questionNumber = 1
     private lateinit var binding: FragmentQuestionBinding
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var db: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +30,7 @@ class QuestionFragment(topicID: String) : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
+        db = FirebaseDatabase.getInstance().reference
         // Inflate the layout for this fragment
         binding = FragmentQuestionBinding.inflate(inflater, container, false)
         mAuth = FirebaseAuth.getInstance()
@@ -33,19 +39,19 @@ class QuestionFragment(topicID: String) : Fragment() {
         binding.questionText.text = getString("topic" + topicID + "question" + questionNumber)
 
         binding.option1.setOnClickListener{
-            nextQuestion("1")
+            nextQuestion(1)
         }
         binding.option2.setOnClickListener{
-            nextQuestion("2")
+            nextQuestion(2)
         }
         binding.option3.setOnClickListener{
-            nextQuestion("3")
+            nextQuestion(3)
         }
         binding.option4.setOnClickListener{
-            nextQuestion("4")
+            nextQuestion(4)
         }
         binding.option5.setOnClickListener{
-            nextQuestion("5")
+            nextQuestion(5)
         }
         return binding.root
     }
@@ -59,15 +65,20 @@ class QuestionFragment(topicID: String) : Fragment() {
     private fun saveSurvey()
     {
         // save to DB
-        val transaction = activity?.supportFragmentManager?.beginTransaction()
-        if (transaction != null) {
-            transaction.replace(this.id, SurveyResultsFragment())
-            transaction.disallowAddToBackStack()
-            transaction.commit()
-        }
+        val agg = (responselist.get(0) + responselist.get(1) + responselist.get(2) + responselist.get(3) + responselist.get(4))/5
+        db.child("Views").child(mAuth.currentUser!!.uid).child(topicID).setValue(ViewResponse(mAuth.currentUser!!.uid, responselist.get(0), responselist.get(1), responselist.get(2), responselist.get(3), responselist.get(4), agg.toFloat())).addOnCompleteListener(
+            OnCompleteListener {
+            Toast.makeText(context, "Answers logged", Toast.LENGTH_SHORT).show()
+                val transaction = activity?.supportFragmentManager?.beginTransaction()
+                if (transaction != null) {
+                    transaction.replace(this.id, SurveyResultsFragment())
+                    transaction.disallowAddToBackStack()
+                    transaction.commit()
+                }
+        })
     }
 
-    private fun nextQuestion(value: String)
+    private fun nextQuestion(value: Int)
     {
         responselist.add(value)
         questionNumber++
---
+++ FireBaseChat/app/src/main/java/com/example/myapplication/ViewResponse.kt
@@ -0,0 +1,69 @@
+package com.example.myapplication
+
+class ViewResponse(
+    private var uid: String = "",
+    private var answer1: Int = 0,
+    private var answer2: Int = 0,
+    private var answer3: Int = 0,
+    private var answer4: Int = 0,
+    private var answer5: Int = 0,
+    private var agg: Float = 0F) {
+
+    fun setUid(uid: String): Unit{
+        this.uid = uid
+    }
+
+    fun setAnswer1(response: Int): Unit{
+        this.answer1 = response
+    }
+
+    fun setAnswer2(response: Int): Unit{
+        this.answer2 = response
+    }
+
+    fun setAnswer3(response: Int): Unit{
+        this.answer3 = response
+    }
+
+    fun setAnswer4(response: Int): Unit{
+        this.answer4 = response
+    }
+
+    fun setAnswer5(response: Int): Unit{
+        this.answer5 = response
+    }
+
+    fun setAgg(agg: Float): Unit{
+        this.agg = agg
+    }
+
+    fun getUID(): String {
+        return this.uid
+    }
+
+    fun getAnswer1(): Int {
+        return this.answer1
+    }
+
+    fun getAnswer2(): Int {
+        return this.answer2
+    }
+
+    fun getAnswer3(): Int {
+        return this.answer3
+    }
+
+    fun getAnswer4(): Int {
+        return this.answer4
+    }
+
+    fun getAnswer5(): Int {
+        return this.answer5
+    }
+
+    fun getAgg(): Float {
+        return this.agg
+    }
+
+
+}
